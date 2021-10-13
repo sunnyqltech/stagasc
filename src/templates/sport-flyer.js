564c1f4e8eb3c45b0b843b29ca60e0bd01fa7846
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2021-10-13 13:41:31 +0530

    Add files via upload

---
+++ src/templates/sport-flyer.js
@@ -0,0 +1,224 @@
+import React, {Component} from 'react';
+import { graphql,Link } from "gatsby"
+import PropTypes from "prop-types"
+import {Container,Breadcrumb} from 'react-bootstrap'
+import {Image,ListGroup,Form,InputGroup,FormControl,Button,Tab,Nav} from 'react-bootstrap'
+import {Row,Col,Card} from 'react-bootstrap'
+import Layout from "../components/layout"
+import Footer from "../components/common/Footer"
+import BottomForm from "../components/common/BottomForm"
+import card1 from '../images/blog-card-1.png'
+import imgbox1 from '../images/blog-imgbox-1.png'
+import imgbox2 from '../images/blog-imgbox-2.png'
+import imgbox3 from '../images/blog-imgbox-3.png'
+import imgbox4 from '../images/blog-imgbox-4.png'
+import sidebaradd1 from '../images/sidebar-add-1.png'
+import sidebaradd2 from '../images/sidebar-add-2.png'
+import { FaSearch } from "react-icons/fa";
+import { Helmet } from "react-helmet"
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+class demopage extends Component {
+	
+	
+componentDidMount() {
+	  
+	/*after page load segment hook*/ 
+	var resourceType = 'Flyer';
+	var path  = '/flyer/';
+	var url_v  = 'https://staging-ascstaging.kinsta.cloud/flyer/';
+	var source = '';
+	var userId = '';
+	const cookies = new Cookies();
+	if(cookies.get('LOGIN') == 1){
+		var login_name = cookies.get('EMAIL');
+	}
+	else{
+		var login_name = "";
+	}
+	 var url = "https://staging-ascstaging.kinsta.cloud/wp-json/newasc/v1/page_view_res";
+		axios.post(url,{resourceType:resourceType,url:url_v,path:path,source:source,userId:login_name}).then(e => {
+			console.log("done");
+		})
+  }	
+	
+  render() {
+    const blog = this.props.data.allWordpressWpCpt150963
+    const blog_count = this.props.data.allWordpressWpCpt150963.edges.length;
+   	const category = this.props.data.allWordpressCategory
+   
+   
+    return (
+		<Layout>
+		<Helmet>
+				<title>Flyers - School Hoilday Programs | Australian Sports Camps</title>
+				<meta name="title" content="Flyers - School Hoilday Programs | Australian Sports Camps"></meta>
+				<meta property="og:title" content="Flyers - School Hoilday Programs | Australian Sports Camps"></meta>
+				<meta property="twitter:title" content="Flyers - School Hoilday Programs | Australian Sports Camps"></meta>
+
+				<meta name="description" content="Get detailed information on all our sports camps planned in major cities. Camps for all major sports in Australia. Kids receive coaching at top facilities."></meta>
+				<meta property="og:description" content="Get detailed information on all our sports camps planned in major cities. Camps for all major sports in Australia. Kids receive coaching at top facilities."></meta>
+				<meta property="twitter:description" content="Get detailed information on all our sports camps planned in major cities. Camps for all major sports in Australia. Kids receive coaching at top facilities."></meta>
+
+				<meta name="keywords" content="Camps Flyer, Sports Camps Flyer, ACT Camps Flyer, Camps Flyer Australia, Summer Camps Flyer, Autumn Camps Flyer, Flyer Camps, Australian Sports Camps, Summer Camps Flyer for schools"></meta>
+
+				<meta property="og:type" content="website"></meta>
+				<meta property="twitter:card" content="summary_large_image"></meta>
+
+
+			</Helmet>
+			<section className="Banner-Section">
+                <Container>
+                    <div className="Banner-Section-data">
+                    <Breadcrumb>
+                        <Breadcrumb.Item className="">
+                            <Link className="nav-link p-0" to="/">Home</Link>
+                            </Breadcrumb.Item>
+						<Breadcrumb.Item className="">
+									<Link className="nav-link p-0" to="/resources">Resources</Link>
+								</Breadcrumb.Item>	
+								<Breadcrumb.Item active className="">
+									<Link className="nav-link p-0" to="/sport-flyer">Sport flyer</Link>
+								</Breadcrumb.Item>		
+                    </Breadcrumb>
+                    <h2 className="heading-banner">Camp flyer</h2>
+                    </div>
+                </Container>
+            </section>
+			<section className="Resource-section-2">
+                <Container>
+                    <div className="Resource-Section-2-data">
+						<Tab.Container id="left-tabs-example" defaultActiveKey="Blog">
+							<div className="p-0 border-0 justify-content-center d-flex mb-30">
+								<Nav variant="pills" className="">
+									<Nav.Item>
+										<Nav.Link  href="/resources" className="uppercase font-bold">All</Nav.Link>
+									</Nav.Item>
+									 <Nav.Item>
+										<Nav.Link  href="/blog" className="uppercase font-bold">Blogs</Nav.Link>
+									</Nav.Item>
+									<Nav.Item>
+										<Nav.Link href="/videos" className="uppercase font-bold">Videos</Nav.Link>
+									</Nav.Item>
+									<Nav.Item>
+										<Nav.Link href="/guide" className="uppercase font-bold ">Guides</Nav.Link>
+									</Nav.Item>
+									<Nav.Item>
+										<Nav.Link href="/flyer" className="uppercase font-bold nav-link">Camp flyer</Nav.Link>
+									</Nav.Item>
+									<Nav.Item>
+										<Nav.Link href="/sport-flyer" className="uppercase font-bold nav-link active">Sport flyer</Nav.Link>
+									</Nav.Item>
+								</Nav>
+							</div>
+							<Tab.Content className="text-left">
+								<Tab.Pane eventKey="Blog">
+									<section className="Resource-guides">
+									{(blog_count > 0) ? (
+										<Container>
+											<div className="Resource-guides-data">
+												<Row className="">
+													
+													{blog && blog.edges.map( prop => {
+														return (
+															<>
+															{(prop.node.acf.active == 1) ? (
+																<Col xl={4} lg={4} md={6} className="mb-30 resource-card-col">
+																	<Card className="resource-card mb-0 all">
+																		<Link to={"/flyer/"+prop.node.slug} className="card-img">
+																			<Image src={prop.node.acf.featured_image} fluid alt="" className="" />
+																		</Link>
+																		<Card.Body>
+																			<Card.Title as="h5" dangerouslySetInnerHTML={{ __html: prop.node.title.substring(0, 50)+"...."}}  />
+																			<Card.Text as="div" className="" dangerouslySetInnerHTML={{ __html: prop.node.content.substring(0, 90)+"..."}} />
+																			<Link className="nav-link p-0 d-flex align-items-center" to={"/flyer/"+prop.node.slug}>Read more 
+																				<i className="fa fa-chevron-right ml-2"></i> 
+																			</Link>
+																		</Card.Body>
+																	</Card>
+																</Col>	
+															
+															) : ("") }
+															</>
+														
+															
+														)
+													})}
+												</Row>
+												<Col xl={12} lg={12} md={12} sm={12}  className="">
+													{(this.props.pageContext.previousPagePath !== "") ? (<Link className="btn uppercase btn-sm btn-orange"  to={this.props.pageContext.previousPagePath}>Previous</Link>) : (<Link className="btn btn-primary invisible"  to={this.props.pageContext.previousPagePath}>Previous</Link>)}
+				
+				
+													{(this.props.pageContext.nextPagePath !== "") ? (<Link className="btn uppercase btn-sm btn-orange next-btnblog" to={this.props.pageContext.nextPagePath}>Next</Link>) : (<Link className="btn btn-primary invisible" to={this.props.pageContext.nextPagePath}>Next</Link>)}
+												</Col>
+											</div>
+										</Container>
+									
+									) : (
+									<Container>
+										<div className="Resource-guides-data">
+											<Row className="">
+												<p className="text-success" > No Sport flyers found at the moment  </p>
+											</Row>
+										</div>
+									</Container>
+									)}
+										
+										
+									</section>
+								</Tab.Pane>
+							</Tab.Content>
+						</Tab.Container>
+                    </div>
+                </Container>
+            </section>
+			<BottomForm/>
+			<Footer/>
+			 
+		 
+		</Layout>
+	)
+	}}
+
+demopage.propTypes = {
+  data: PropTypes.object.isRequired,
+  edges: PropTypes.array,
+}
+
+export default demopage
+
+
+export const pageQuery = graphql`
+	query($skip: Int!, $limit: Int!) {
+		allWordpressWpCpt150999(skip: $skip, limit: $limit) {
+		edges {
+			node {
+				id
+				acf {
+					featured_image
+					author_name
+					active
+				}
+				
+				slug
+				title
+				date(formatString: "D MMMM, Y")
+				content
+			}
+		}
+	}
+	allWordpressCategory {
+		edges {
+			node {
+				id
+				name
+				slug
+				count
+			}
+		}
+	}
+  }
+  
+`
+
---
+++ src/templates/sport-flyer_details.js
@@ -0,0 +1,220 @@
+import React, {Component} from 'react';
+import Layout from "../components/layout"
+import Footer from "../components/common/Footer"
+import BottomForm from "../components/common/BottomForm"
+import Guidebanner from "../components/common/Guidebanner"
+import {Container,Breadcrumb} from 'react-bootstrap'
+import {Row,Col,Card,Image} from 'react-bootstrap'
+import { Link,graphql } from 'gatsby'
+import sidebaradd1 from '../images/sidebar-add-1.png'
+import sidebaradd2 from '../images/sidebar-add-2.png'
+import PropTypes from "prop-types"
+import { Disqus } from 'gatsby-plugin-disqus'
+import { Helmet } from "react-helmet"
+class BlogDetails extends Component {
+	constructor(props) {
+        super(props);
+        this.state={
+            currentURL:"",
+        }
+       
+    }
+	componentDidMount() {
+		
+		this.setState({currentURL: window.location.href})
+		
+    }
+	
+	
+  render() {
+  
+	const blog = this.props.data.allWordpressWpCpt150963
+	const id = this.props.data.allWordpressWpCpt150963.edges[0].node.wordpress_id
+	const sport_name = this.props.data.allWordpressWpCpt150963.edges[0].node.acf.sport_name
+	const title = this.props.data.allWordpressWpCpt150963.edges[0].node.title
+	 	const category = this.props.data.allWordpressCategory
+   	const tags = this.props.data.allWordpressTag
+	
+	let disqusConfig = {
+		url: "",
+		identifier: id,
+		title: title,
+	}
+      return (
+          <Layout>
+            <>
+			<Helmet>
+				<title>{blog.edges[0].node.title} - Australian Sports Camps</title>
+					<meta name="title" content={blog.edges[0].node.yoast.title}></meta>
+			<meta name="description" content={blog.edges[0].node.yoast.metadesc}></meta>
+			 <meta name="keywords" content={blog.edges[0].node.yoast.metakeywords}></meta>
+			<meta property="og:type" content="website"></meta>
+			<meta property="og:title" content={blog.edges[0].node.yoast.opengraph_title}></meta>
+			<meta property="og:description" content={blog.edges[0].node.yoast.opengraph_description}></meta>
+			<meta property="twitter:card" content="summary_large_image"></meta>
+			<meta property="twitter:title" content={blog.edges[0].node.yoast.title}></meta>
+			<meta property="twitter:description" content={blog.edges[0].node.yoast.twitter_description}></meta>
+			</Helmet>
+				<section className="Banner-Section">
+					<Container>
+						<div className="Banner-Section-data">
+							<Breadcrumb>
+								<Breadcrumb.Item className="">
+									<Link className="nav-link p-0" to="/">Home</Link>
+								</Breadcrumb.Item>
+								<Breadcrumb.Item className="">
+									<Link className="nav-link p-0" to="/flyer">Flyer</Link>
+								</Breadcrumb.Item>
+								<Breadcrumb.Item active href="" dangerouslySetInnerHTML={{ __html: blog.edges[0].node.title}} className="" />
+							</Breadcrumb>
+							<h2 className="heading-banner">Flyer</h2>
+						</div>
+					</Container>
+				</section>
+				<section className="Blog-details-Section-2">
+					<Container>
+						<div className="Blog-details-Section-2-data">
+							<Row>
+								<Col xl={8} lg={8} md={8} className="lg-mb-2">
+									<Card className="blog-details-card mb-30">
+										<Card.Title as="h5" dangerouslySetInnerHTML={{ __html: blog.edges[0].node.title}} />
+										<span> by {blog.edges[0].node.acf.author_name} / { blog.edges[0].node.date}</span>
+										<div className="card-img mb-3">
+											<Image src={ blog.edges[0].node.acf.featured_image} fluid />
+										</div>
+										<Card.Text as="div" dangerouslySetInnerHTML={{ __html: blog.edges[0].node.content}} />
+									</Card>
+								</Col>
+								<Col xl={4} lg={4} md={4} className="lg-mb-2">
+									<iframe height="450" width="100%" frameBorder="0"  src={"https://staging-ascstaging.kinsta.cloud/gravity-flyer/?form_page="+sport_name+"&id="+id+"&url="+this.state.currentURL} title="description" />
+									
+									<Guidebanner />
+								</Col>
+							</Row>
+						</div>
+						<div className="clearfix nav-controls border rounded mb-4">
+                            {(blog.edges[0].previous != null) ? (<Link to={"guide/"+blog.edges[0].previous.slug+"/"} className="nav-control-left float-left  rounded p-1">
+                               <span className="label-text  d-block">Previous Post</span>
+                               <span className="section-heading-3  mb-0">{blog.edges[0].previous.title}</span>
+                            </Link>) : ('')}
+							
+							{(blog.edges[0].next != null) ? (<Link to={"guide/"+blog.edges[0].next.slug+"/"} className="nav-control-left float-right text-right  rounded p-1">
+                               <span className="label-text  d-block">Next Post</span>
+                               <span className="section-heading-3  mb-0">{blog.edges[0].next.title}</span>
+                            </Link>) : ('')}
+							
+                        </div>
+							<Disqus config={disqusConfig} />
+	  
+					</Container>
+				</section>
+				{(blog.edges[0].node.acf.footer_title != "") ? (
+					<section className="Resource-details-section-2">
+						<Image src={ blog.edges[0].node.acf.footer_image} />
+						<div className="container">
+							<div className="Resource-details-Section-data">
+								<div className="row justify-content-end">
+									<div className="col-xl-5 col-lg-6 col-md-10">
+										<div className="title text-left">
+											<h2>{ blog.edges[0].node.acf.footer_title} </h2>
+											<div dangerouslySetInnerHTML={{ __html: blog.edges[0].node.acf.footer_desc}} />
+										</div>
+										<div className="text-center">
+											<button className=" uppercase btn-orange-border">{ blog.edges[0].node.acf.footer_button_text}</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				) : ("")}
+				
+				
+      
+              <BottomForm/>
+              <Footer/>
+            </>
+          </Layout>
+      )
+  }
+}
+
+BlogDetails.propTypes = {
+  data: PropTypes.object.isRequired,
+  edges: PropTypes.array,
+}
+
+export default BlogDetails
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    allWordpressWpCpt150999(filter: {id: { eq: $id }}) {
+		edges {
+			next {
+				slug
+				title
+			}	
+			previous {
+				slug
+				title
+			}
+			node {
+				id
+				acf {
+					featured_image
+					footer_title
+					footer_desc
+					footer_image
+					footer_button_text
+					author_name
+					sport_name
+				}
+				yoast {
+				focuskw
+				linkdex
+				meta_robots_adv
+				meta_robots_nofollow
+				meta_robots_noindex
+				metadesc
+				metakeywords
+				opengraph_description
+				opengraph_image
+				opengraph_title
+				redirect
+				title
+				twitter_description
+				twitter_image
+			  }
+				date(formatString: "D MMMM, Y")
+				title
+				content
+				wordpress_id
+				slug
+			}
+		}
+    }
+	allWordpressCategory {
+		edges {
+			node {
+				id
+				name
+				slug
+				count
+			}
+		}
+	}
+	allWordpressTag {
+		edges {
+			node {
+				id
+				name
+				count
+				slug
+			}
+		}
+	}
+	
+	
+  }
+  
+`
